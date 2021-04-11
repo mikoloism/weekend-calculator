d0b83f69318e613666b34c0d793693d17dcd80ce
miko-github <mikoloism.github@gmail.com>
2021-04-11 20:26:26 +0430

    import wg-calc comp, then remove no need more lines

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import WgCalc from './components/wg-calc/WgCalc';
+import './styles/main.scss';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [state, setState] = useState({});
+	return <WgCalc store={{ state, setState }} />;
 }
 
 export default App;
