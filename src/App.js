b89521fd86ff178b3c335ff986f430dc0f0bbb42
miko-github <mikoloism.github@gmail.com>
2021-04-12 04:17:31 +0430

    use App.js file as Store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import WgCalc from './components/wg-calc/WgCalc';
 import './styles/main.scss';
 import { useState } from 'react';
-
+const Store = { state: null, setState: null };
 function App() {
-	const [state, setState] = useState({});
+	const [state, setState] = useState({
+		textInputed: '0',
+		valueInputed: 0,
+	});
+	Store.state = state;
+	Store.setState = setState;
 	return <WgCalc store={{ state, setState }} />;
 }
 
+export { Store };
 export default App;
